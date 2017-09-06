a731bbe37235338cd6ca2326173757bc25962f0b
omatthew98 <omatthew98@berkeley.edu>
2017-09-05 23:09:47 -0700

    initial structure

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var yourName = prompt("Please enter your name:");
-
-if(yourName !== null) {
-	document.getElementById("header").innerHTML = "This site is owned by: " + yourName;
-} else {
-	alert("Please enter a name ya fool");
-}
-
-var buttonState = false;
-
-function writeText() {
-	if(!buttonState){
-		document.getElementById("paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis finibus nisl id molestie ornare. Cras sed mi eu lorem mollis consectetur vel at diam. Integer lectus risus, maximus at vulputate ut, vestibulum et est. Duis a magna dui. Mauris scelerisque rutrum sem, sed suscipit dui placerat sed. Quisque id felis a lacus imperdiet lacinia id eu velit. Nunc cursus venenatis nunc at efficitur. Duis tortor purus, sollicitudin vel quam vitae, egestas eleifend augue. Mauris quam justo, laoreet id sodales interdum, congue sit amet neque. Morbi hendrerit mi neque, a elementum odio tincidunt at. <br/><br/>Vestibulum lacus augue, pulvinar ac arcu vitae, egestas aliquam lectus. Sed porta, lacus fringilla elementum tincidunt, ipsum sem efficitur elit, ac imperdiet nulla elit in tellus. Mauris vel enim commodo, ullamcorper nisl id, condimentum massa. Vestibulum commodo enim id nibh congue dignissim. \n Mauris ac dictum dolor. Vestibulum iaculis vel tellus quis luctus. Aliquam mollis non nisl et fringilla. Suspendisse mattis lacinia sollicitudin. Curabitur id scelerisque turpis. Quisque euismod vitae ipsum nec ultrices. Aenean maximus urna euismod maximus placerat. Aliquam nec auctor ligula, ut tristique neque. Sed scelerisque facilisis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer non blandit lorem, gravida blandit purus. Suspendisse potenti. Mauris lacinia ex eu diam vestibulum scelerisque. Integer sapien elit, accumsan quis faucibus finibus, ullamcorper sed est. <br/><br/>In tincidunt eros augue, sit amet sagittis justo imperdiet et. Aenean sollicitudin malesuada hendrerit.\nCurabitur non sapien euismod, pharetra metus at, blandit justo. Nam tortor nisl, vulputate eu nibh sit amet, convallis dictum dolor. Ut auctor dui vitae leo efficitur, non vulputate nulla accumsan. Phasellus eu nisi vel quam interdum rhoncus non sit amet neque. Nullam fringilla imperdiet nulla ac lobortis. Sed ut porta quam. Nunc sagittis nibh feugiat, finibus dolor vel, pretium velit. Sed bibendum, mi bibendum tristique sollicitudin, libero lacus congue neque, ac venenatis nisi urna ut lacus.";
-		document.getElementById("button").innerHTML = "Show less";
-	} else {
-		document.getElementById("paragraph").innerHTML = "";
-		document.getElementById("button").innerHTML = "Show more";		
-	}
-	buttonState = !buttonState;
-}
+//var yourName = prompt("Please enter your name:");
+//
+//if(yourName !== null) {
+//	document.getElementById("header").innerHTML = "This site is owned by: " + yourName;
+//} else {
+//	alert("Please enter a name ya fool");
+//}
+//
+//var buttonState = false;
+//
+//function writeText() {
+//	if(!buttonState){
+//		document.getElementById("paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis finibus nisl id molestie ornare. Cras sed mi eu lorem mollis consectetur vel at diam. Integer lectus risus, maximus at vulputate ut, vestibulum et est. Duis a magna dui. Mauris scelerisque rutrum sem, sed suscipit dui placerat sed. Quisque id felis a lacus imperdiet lacinia id eu velit. Nunc cursus venenatis nunc at efficitur. Duis tortor purus, sollicitudin vel quam vitae, egestas eleifend augue. Mauris quam justo, laoreet id sodales interdum, congue sit amet neque. Morbi hendrerit mi neque, a elementum odio tincidunt at. <br/><br/>Vestibulum lacus augue, pulvinar ac arcu vitae, egestas aliquam lectus. Sed porta, lacus fringilla elementum tincidunt, ipsum sem efficitur elit, ac imperdiet nulla elit in tellus. Mauris vel enim commodo, ullamcorper nisl id, condimentum massa. Vestibulum commodo enim id nibh congue dignissim. \n Mauris ac dictum dolor. Vestibulum iaculis vel tellus quis luctus. Aliquam mollis non nisl et fringilla. Suspendisse mattis lacinia sollicitudin. Curabitur id scelerisque turpis. Quisque euismod vitae ipsum nec ultrices. Aenean maximus urna euismod maximus placerat. Aliquam nec auctor ligula, ut tristique neque. Sed scelerisque facilisis vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer non blandit lorem, gravida blandit purus. Suspendisse potenti. Mauris lacinia ex eu diam vestibulum scelerisque. Integer sapien elit, accumsan quis faucibus finibus, ullamcorper sed est. <br/><br/>In tincidunt eros augue, sit amet sagittis justo imperdiet et. Aenean sollicitudin malesuada hendrerit.\nCurabitur non sapien euismod, pharetra metus at, blandit justo. Nam tortor nisl, vulputate eu nibh sit amet, convallis dictum dolor. Ut auctor dui vitae leo efficitur, non vulputate nulla accumsan. Phasellus eu nisi vel quam interdum rhoncus non sit amet neque. Nullam fringilla imperdiet nulla ac lobortis. Sed ut porta quam. Nunc sagittis nibh feugiat, finibus dolor vel, pretium velit. Sed bibendum, mi bibendum tristique sollicitudin, libero lacus congue neque, ac venenatis nisi urna ut lacus.";
+//		document.getElementById("button").innerHTML = "Show less";
+//	} else {
+//		document.getElementById("paragraph").innerHTML = "";
+//		document.getElementById("button").innerHTML = "Show more";		
+//	}
+//	buttonState = !buttonState;
+//}
